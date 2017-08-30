2a2944f8d3b70e8876c771a246e0baa3d145075b
Alberto Marín <alberto.marin@wazuh.com>
2017-08-30 19:58:43 +0200

    Syscollector JSON decoder

--- src/wazuh_modules/syscollector/syscollector_windows.c
+++ src/wazuh_modules/syscollector/syscollector_windows.c
@@ -73,7 +73,7 @@ void sys_hw_windows(const char* LOCATION){
     /* Send interface data in JSON format */
     string = cJSON_PrintUnformatted(object);
     mtdebug2(WM_SYS_LOGTAG, "sys_hw_windows() sending '%s'", string);
-    SendMSG(0, string, LOCATION, WODLE_MQ);
+    SendMSG(0, string, LOCATION, SYSCOLLECTOR_MQ);
     cJSON_Delete(object);
 
     free(string);
@@ -93,7 +93,7 @@ void sys_os_windows(const char* LOCATION){
     /* Send interface data in JSON format */
     string = cJSON_PrintUnformatted(object);
     mtdebug2(WM_SYS_LOGTAG, "sys_os_windows() sending '%s'", string);
-    SendMSG(0, string, LOCATION, WODLE_MQ);
+    SendMSG(0, string, LOCATION, SYSCOLLECTOR_MQ);
     cJSON_Delete(object);
 
     free(string);
@@ -345,7 +345,7 @@ void sys_network_windows(const char* LOCATION){
 
             string = cJSON_PrintUnformatted(object);
             mtdebug2(WM_SYS_LOGTAG, "sys_network_windows() sending '%s'", string);
-            SendMSG(0, string, LOCATION, WODLE_MQ);
+            SendMSG(0, string, LOCATION, SYSCOLLECTOR_MQ);
             cJSON_Delete(object);
 
             free(string);
